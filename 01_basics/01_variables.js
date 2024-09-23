b74d974f19d89f696b7a923d72d342e271df817e
Ashish Kumar Arya <ashish2018ku@gmail.com>
2024-09-23 12:39:28 +0000

    01_variables update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "ashish@google.com"
+var accountPassword = "12345"
+accountCity = "Haldwani"
+// accountId = 12221 // not allowed
+console.log(accountId);
