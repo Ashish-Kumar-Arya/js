2ffb3c6b40de54f25860b4f7fef48ccad2d92ea0
Ashish Kumar Arya <ashish2018ku@gmail.com>
2024-09-24 06:10:08 +0000

    datatype

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,9 +1,12 @@
 "use strict"; // treat all JS code as newer version
 
 let name = "Ashish" // string
-let age = 22 // int
+let age = 22 // number
 let isLoggedIn = false // boolean
 let car = null // standalone value
 let planet; // undefined
 // symbol => unique
-// object
+// object  
+console.log(typeof "Ashish")
+console.log(typeof null) // type is object
+console.log(typeof undefined) // type is undefined
