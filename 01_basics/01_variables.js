bdebb40fa38dec631916ad900204ac499e4cc013
Ashish Kumar Arya <ashish2018ku@gmail.com>
2024-09-24 06:01:16 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,7 @@ let accountEmail = "ashish@google.com"
 var accountPassword = "12345"
 accountCity = "Haldwani"
 // accountId = 12221 // not allowed
-console.log(accountId);
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Ashish" // string
+let age = 22 // int
+let isLoggedIn = false // boolean
+let car = null // standalone value
+let planet; // undefined
+// symbol => unique
+// object
